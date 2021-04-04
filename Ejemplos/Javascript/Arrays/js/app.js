84fb0f830abac0bae57e659d0df2f862f62588d5
Riardo Daniel Palomares Arriaga <ricpalo001@gmail.com>
2021-04-03 22:07:18 -0600

    Arrays Terminados

---
+++ Ejemplos/Javascript/Arrays/js/app.js
@@ -0,0 +1,27 @@
+let variable1 = [10, 20, 30, 40]; // Crear un array
+
+console.log(variable1);
+
+variable1.push(50); // Añadir un elemento al final del array
+
+console.log(variable1);
+
+variable1.pop(); // Eliminar el último elemento del array
+
+console.log(variable1);
+
+variable1.unshift(0); // Añadir un elemento al inicio del array
+
+console.log(variable1);
+
+variable1.shift(); // Eliminar el primer elemento del array
+
+console.log(variable1);
+
+console.log(variable1.length); // Determinar la cantidad de elementos que se encuentran en el array
+
+console.log(variable1[0]); // Obtener el primer elemento del array
+
+console.log(variable1[variable1.length - 1]); // Obtener el último elemento del array
+
+console.log(variable1.indexOf(30)); // Encontrar la posición de un elemento
