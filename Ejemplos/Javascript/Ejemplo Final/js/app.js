c8c161ddb1f6167a567f1807b856dcdf98d33ce3
Riardo Daniel Palomares Arriaga <ricpalo001@gmail.com>
2021-04-04 00:12:38 -0600

    Eventos Agregados

--- Ejemplos/Javascript/Ejemplo Final/js/app.js
+++ Ejemplos/Javascript/Ejemplo Final/js/app.js
@@ -1,8 +1,36 @@
 document.addEventListener('DOMContentLoaded', function(){
-    eventos();
+    eventos1();
+    eventos2();
+    eventos3();
 });
 
-function eventos(){
+function eventos1(){
+    const titulo = document.querySelector('h1');
+
+    if(titulo){
+        console.log('Existe');
+    } else{
+        console.log('No existe');
+    }
+}
+
+function eventos2(){
+    const titulo = document.querySelector('h1');
+
+    // titulo.addEventListener('mouseover', function(){
+    //     console.log('Mouse Encima');
+    // });
+
+    // titulo.addEventListener('mouseout', function(){
+    //     console.log('Mouse Fuera');
+    // });
+
+    titulo.addEventListener('click', function(){
+        console.log('Click Al Elemento');
+    });
+}
+
+function eventos3(){
     const titulo = document.querySelector('h1');
 
     titulo.addEventListener('click', function(){
--- Ejemplos/Javascript/Variables/js/app.js
+++ Ejemplos/Javascript/Variables/js/app.js
@@ -1,8 +1,13 @@
 var variable1;
 let variable2 = 3;
 const variable3 = 'Hola';
+// variable3 = 'Adiós';
 
 variable1 = 40;
 variable2 = 10;
 
-variable3 = 10;
+let variable4 = variable2 + 10;
+
+let variable5 = true;
+
+console.log(variable5);
