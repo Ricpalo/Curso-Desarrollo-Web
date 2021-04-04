b1d81527d4b3805ffd4ba603be181a5232f76b8d
Riardo Daniel Palomares Arriaga <ricpalo001@gmail.com>
2021-04-03 23:04:33 -0600

    Funciones Agregadas

---
+++ Ejemplos/Javascript/Funciones/js/app.js
@@ -0,0 +1,21 @@
+// function decirHola(){
+//     console.log('Hola');
+// }
+
+// decirHola();
+
+
+// let valor = 100;
+
+// function decirValor(valor){
+//     console.log(`Usted tiene ${valor} pesos`);
+// }
+
+// decirValor(valor);
+
+
+function suma(numero1, numero2){
+    return numero1 + numero2;
+}
+
+console.log(suma(10, 10));
